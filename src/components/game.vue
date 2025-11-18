92c530ed9dce031aa48a602e17fd8b229be68b62
giba-dev127 <mivmestenaveki@gmail.com>
2025-11-18 21:37:30 +0500

    Update game.vue
    Co-authored-by: andrronov andrew270504cska@gmail.com

--- src/components/game.vue
+++ src/components/game.vue
@@ -1,36 +1,3 @@
-<template>
-   <div v-if="log" class="fixed inset-0 flex items-center justify-center bg-red-600 text-white text-2xl">
-      {{ log }}
-   </div>
-   <div v-if="users.length < 2" class="fixed inset-0 flex flex-col gap-4 items-center justify-center bg-black text-white text-2xl">
-      <p>Waiting for second player...</p>
-      <button @click="router.push('/lobby')" class="mt-2 p-1 text-white border border-white hover:bg-gray-800">Back to lobby</button>
-   </div>
-   <div class="w-full max-w-7xl mx-auto h-screen flex justify-between py-2 items-center flex-col">
-      <div class="w-full flex flex-row items-center justify-around">
-         <p class="text-white" v-for="(user, index) in users" :key="index">{{user}}</p>
-      </div>
-      <p v-if="users.length > 0" class="text-white">{{ store.moveIndex % 2 == 0 ? `User ${users[0]} move` : `User ${users[1]} move` }}</p>
-      <div class="grid grid-cols-3 gap-2 w-full s:w-1/2 max-w-7xl mx-auto h-1/2 justify-center">
-         <div v-for="(item, index) in matrix" :key="index" @click="item.length < 1 && isCanMove ? userMove(index) : ''" class="place-self-center w-24 h-24 cursor-pointer text-2xl flex items-center justify-center bg-gray-400">
-            {{ item }}
-         </div>
-      </div>
-      <div v-if="isWin" @click="restartGame" class="p-2 border border-white text-white text-xl text-center w-60 cursor-pointer hover:bg-white hover:text-black">
-         <p>Winner: {{ isWin }}</p>
-         <p>Restart?</p>
-      </div>
-      <div v-if="isMatrixFull && !isWin" @click="restartGame" class="p-2 border border-white text-white text-xl text-center w-60 cursor-pointer hover:bg-white hover:text-black">
-         <p>Draw</p>
-         <p>Restart?</p>
-      </div>
-
-      <div class="flex flex-col items-center gap-3">
-         <button v-if="!isMatrixFull && !isWin" @click="restartGame" class="mt-2 p-1 text-white border border-white hover:bg-gray-800">Restart</button>
-         <button @click="router.push('/lobby')" class="mt-2 p-1 text-white border border-white hover:bg-gray-800">Back to lobby</button>
-      </div>
-   </div>
-</template>
 <script setup>
 import {computed, onBeforeUnmount, onMounted, ref, watch, watchEffect} from 'vue'
 import {useRoute, useRouter} from 'vue-router'
@@ -175,3 +142,37 @@ onBeforeUnmount(() => {
    socket.send(JSON.stringify({method: 'leave', username: localStorage.getItem('username')}))
 })
 </script>
+
+<template>
+   <div v-if="log" class="fixed inset-0 flex items-center justify-center bg-red-600 text-white text-2xl">
+      {{ log }}
+   </div>
+   <div v-if="users.length < 2" class="fixed inset-0 flex flex-col gap-4 items-center justify-center bg-black text-white text-2xl">
+      <p>Waiting for second player...</p>
+      <button @click="router.push('/lobby')" class="mt-2 p-1 text-white border border-white hover:bg-gray-800">Back to lobby</button>
+   </div>
+   <div class="w-full max-w-7xl mx-auto h-screen flex justify-between py-2 items-center flex-col">
+      <div class="w-full flex flex-row items-center justify-around">
+         <p class="text-white" v-for="(user, index) in users" :key="index">{{user}}</p>
+      </div>
+      <p v-if="users.length > 0" class="text-white">{{ store.moveIndex % 2 == 0 ? `User ${users[0]} move` : `User ${users[1]} move` }}</p>
+      <div class="grid grid-cols-3 gap-2 w-full s:w-1/2 max-w-7xl mx-auto h-1/2 justify-center">
+         <div v-for="(item, index) in matrix" :key="index" @click="item.length < 1 && isCanMove ? userMove(index) : ''" class="place-self-center w-24 h-24 cursor-pointer text-2xl flex items-center justify-center bg-gray-400">
+            {{ item }}
+         </div>
+      </div>
+      <div v-if="isWin" @click="restartGame" class="p-2 border border-white text-white text-xl text-center w-60 cursor-pointer hover:bg-white hover:text-black">
+         <p>Winner: {{ isWin }}</p>
+         <p>Restart?</p>
+      </div>
+      <div v-if="isMatrixFull && !isWin" @click="restartGame" class="p-2 border border-white text-white text-xl text-center w-60 cursor-pointer hover:bg-white hover:text-black">
+         <p>Draw</p>
+         <p>Restart?</p>
+      </div>
+
+      <div class="flex flex-col items-center gap-3">
+         <button v-if="!isMatrixFull && !isWin" @click="restartGame" class="mt-2 p-1 text-white border border-white hover:bg-gray-800">Restart</button>
+         <button @click="router.push('/lobby')" class="mt-2 p-1 text-white border border-white hover:bg-gray-800">Back to lobby</button>
+      </div>
+   </div>
+</template>
