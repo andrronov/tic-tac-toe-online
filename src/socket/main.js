2968d4ca0d1579403f88ea1c0fc58b2bab917fb0
giba-dev127 <mivmestenaveki@gmail.com>
2025-11-18 21:32:32 +0500

    clean socket/main

--- src/socket/main.js
+++ src/socket/main.js
@@ -1,13 +1,3 @@
 const socket = new WebSocket('wss://tic-tac-toe-uusw.onrender.com')
-// socket.onopen = () => {
-//    socket.send(JSON.stringify({
-//       method: 'connection',
-//       id: null,
-//       username: null
-//    }))
-// }
-// socket.onmessage = (event) => {
-//    console.log('msg from server', event.data);
-// }
 
 export default socket
